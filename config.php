96d9fdc4fca824ec0d25e264bf3746dc5b64c37e
hajime <tomo2@juno.dti.ne.jp>
2022-08-01 12:26:44 +0900

    説明修正

--- config.php
+++ config.php
@@ -12,12 +12,12 @@ return [
     'description' => 'Remember to replace this description',
     'base_uri' => 'ipfs://NewUriToReplace',
     'image' => [
-        'width' => 200,
-        'height' => 200,
+        'width' => 200, // px
+        'height' => 200, // px
     ],
     'generate_num' => 5, // 生成数
     'file_name_delimiter' => '#',
-    'image_delay' => 50, // 画像切り替え(msec)
+    'image_delay' => 20, // 画像切り替え(100で1秒に1枚。12FPSにするなら、100 * (12 / 60)で20)
     'no_motion_layers' => ['background'],
     'layersOrder' => [
         'background',
