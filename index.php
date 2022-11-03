cb642242aba435e4d78a282a85d40a945a6911fe
hajime001 <hajime00000001@gmail.com>
2022-11-03 19:15:01 +0700

    並列実行用にコンフィグを引数で取るように修正

--- index.php
+++ index.php
@@ -5,8 +5,12 @@ require_once('./class/ImageMixer.php');
 
 ini_set('memory_limit', '8192M');
 
+$imageMixer = new ImageMixer($configs[$argv[1]]);
+$imageMixer->execute(true);
+
+
 foreach($configs as $config) {
-    $imageMixer = new ImageMixer($config);
-    $imageMixer->execute();
+//    $imageMixer = new ImageMixer($config);
+//    $imageMixer->execute(true);
 }
 echo "success!\n";
